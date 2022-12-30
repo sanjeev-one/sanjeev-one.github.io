23481e8974ca1a3c3aaa5ec1725d88fc48568b60
dh2di3oj2 <notfakeelectronicmailservice@gmail.com>
2022-12-30 18:42:47 -0500

    lkjhdfjkagflk

--- src/data/info.ts
+++ src/data/info.ts
@@ -8,11 +8,12 @@ export const info = {
   experience: [
     {
       name: "Duke Aeroelasticity Group",
+      liveUrl: "https://aeroelasticity.pratt.duke.edu/people/sanjeev-chauhan",
       location: "Duke University",
       startDate: "May 2022",
       endDate: "Present",
       description: [
-        "Sanjeev is performing Computational Fluid Dynamics simulations aimed at exploring buffet in transonic flow. Buffet is often explored with the Navier-Stokes equations, but those are computationally intensive. On the other hand, the Euler Equations, which are less computationally intensive, do not account for viscous effects, but may still offer reasonable predictions of buffet onset at a lower computational cost, while giving insight into viscosity’s impact on the phenomenon.",
+        "I am performing Computational Fluid Dynamics simulations aimed at exploring buffet in transonic flow. Buffet is often explored with the Navier-Stokes equations, but those are computationally intensive. On the other hand, the Euler Equations, which are less computationally intensive, do not account for viscous effects, but may still offer reasonable predictions of buffet onset at a lower computational cost, while giving insight into viscosity’s impact on the phenomenon.",
       ],
     },
 
