4913c494fba49e62816204053798c7ebc1b24e9d
Sanjeev <notfakeelectronicmailservice@gmail.com>
2023-10-17 22:38:34 -0400

    update info

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,41 +1,49 @@
 export const info = {
   baseUrl: "https://sanjeev.one/",
   name: "Sanjeev Chauhan",
-  jobDescription: "Aeroelasticity researcher",
-  about: `a Student at Duke University and a researcher at the Duke Aeroelasticity Group.
-  `,
+  jobDescription: "Machine Learning Researcher, Founder and CTO, Computational Fluid Dynamics Researcher",
+  about: `Currently pursuing a B.S. in Electrical and Computer Engineering and Computer Science at Duke University, I have a strong passion for computational research, entrepreneurship, and robotics. With my academic pursuits complemented by hands-on research and leadership experiences, I aim to drive innovation at the intersection of technology and real-world challenges.`,
 
   experience: [
     {
-      name: "Duke Aeroelasticity Group",
-      liveUrl: "https://aeroelasticity.pratt.duke.edu/people/sanjeev-chauhan",
-      location: "Duke University",
+      name: "Stanford Linear Accelerator Center National Lab",
+      location: "Stanford, CA",
+      startDate: "Jun 2023",
+      endDate: "Aug 2023",
+      description: [
+        "Re-engineered FACET-II's computer simulation infrastructure, implementing algorithmic control to improve efficiency.",
+        "Developed a fast machine learning model for the FACET-II photo injector, achieving a speed up of 26,000X vs traditional simulations.",
+        "Recognized for exceptional performance and innovation, earning a return offer to continue spearheading machine learning research.",
+        "Leveraged a technology stack consisting of XOPT, MATLAB, and OpenPMD. Employed Bayesian optimization and Controlled Non-dominated Sorting Genetic Algorithm for advanced simulation optimizations."
+      ],
+    },
+    {
+      name: "DeAP Learning Labs",
+      location: "Durham, NC",
+      startDate: "April 2023",
+      endDate: "Current",
+      description: [
+        "Founded and technically directed an AI-driven educational startup aimed at transforming traditional education through interactive AI.",
+        "Developed custom, scalable RAG-based generative AI API on AWS, enabling chatbots to be trained on history, gov, and econ textbooks.",
+        "Introduced 'talking textbooks': AI-driven chatbots that engage with students using the course material, turning textbooks into responsive tutors.",
+        "Integrated AI tools to grade AP test questions, assist students in answering worksheet questions, and enable chatbots to learn from teacher-provided content.",
+        "Collaborated with notable education Youtubers like Heimler's History, integrating AI versions of their personalities into their online courses.",
+        "Committed to turning education into an immersive, enjoyable experience where subjects come to life, fostering deeper engagement and understanding among students.",
+        "Watch our vision and approach in this [video](https://www.youtube.com/watch?v=qk0ucMmoJa4)."
+      ],
+    },
+    
+    {
+      name: "Duke Aeroelasticity Lab",
+      location: "Durham, NC",
       startDate: "May 2022",
-      endDate: "Present",
+      endDate: "Current",
       description: [
-        "I am performing Computational Fluid Dynamics simulations aimed at exploring buffet in transonic flow. Buffet is often explored with the Navier-Stokes equations, but those are computationally intensive. On the other hand, the Euler Equations, which are less computationally intensive, do not account for viscous effects, but may still offer reasonable predictions of buffet onset at a lower computational cost, while giving insight into viscosity’s impact on the phenomenon.",
+        "Conducted grad-level CFD research on transonic airflow over a wing.",
+        "Investigated use of Euler equations as an alternative to Navier-Stokes equations."
       ],
     },
-
-    {
-      name: "Resbench: HPC Deep Learning Benchmark",
-      location: "University of South Carolina",
-      startDate: "May 2021",
-      endDate: "Dec 2021",
-      description: [
-        "Researched Deep Learning frameworks for image recognition. Used NVIDIA's DL examples, and benchmarked 3 frameworks on 3 datasets using ResNet50V1.5",
-      ],
-    },
-
-    {
-      name: "Freelance Photographer",
-      location: "Duke University",
-      startDate: "Aug 2022",
-      endDate: "Present",
-      description: [
-        "Took Professional Headshots at the Duke Chapel for students to use for LinkedIn profiles and other business needs.",
-      ],
-    },
+    // ... Add other experiences here
   ],
 
   education: [
@@ -43,23 +51,27 @@ export const info = {
       name: "Duke University",
       location: "Durham, NC",
       startDate: "2022",
-      endDate: "2026",
+      endDate: "May 2025",
       description: [
-        "ECE or MechE Major. Researching transonic buffet flow for the Duke Aeroelasticity Lab.",
+        "B.S. in Electrical and Computer Engineering and Computer Science.",
+        "Innovation & Entrepreneurship certificate.",
+        "GPA: 3.78/4.00.",
+        "Relevant Coursework: Machine Learning, Linear Algebra, and more."
       ],
     },
     {
       name: "South Carolina Governor's School for Science and Math",
       location: "Hartsville, SC",
       startDate: "2020",
-      endDate: "2022",
-      description: ["Recieved 2022 Robotics Award. Built a Vehicle-Agnostic Control Systems for Autonomous Vehicle Fleets. https://autogolfcart.github.io/ "],
+      endDate: "May 2022",
+      description: [
+        "Honors: SCGSSM Robotics Award, FRC Design Award S.E. region and more.",
+        "Built a Vehicle-Agnostic Control Systems for Autonomous Vehicle Fleets. https://autogolfcart.github.io/ "
+      ],
     },
-    
   ],
 
   socialMedia: {
-    
     github: "https://github.com/sanjeev-one",
     email: "sanjeev.chauhan@duke.edu",
     linkedin: "https://www.linkedin.com/in/sanjeev-chauhan-a5b570240/",
@@ -74,19 +86,6 @@ export const info = {
       githubUrl: "https://github.com/AutoGolfCart",
       liveUrl: "https://golfcart.sanjeev.one/",
     },
-    {
-      title: "Rebuilt DJI SPARK Drone",
-      isFeatured: true,
-      thumbnail: "/assets/images/drone.jpeg",
-      liveUrl: "https://golfcart.sanjeev.one/",
-    },
-    {
-      title: "FRC robotics",
-      isFeatured: true,
-      thumbnail: "/assets/images/robot.jpeg",
-      liveUrl: "https://golfcart.sanjeev.one/",
-    },
-  
-  
+    // ... Add other projects here
   ],
 };
