cba01e32b9a3817351b7e779a07810ee707a98b5
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2024-03-27 16:59:37 -0400

    added script

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import Script from "next/script"
+
+export default function MyComponent() {
+
+return ( <>
+
+<Script id="octolane-script" src="https://cdn.octolane.com/tag.js?pk=75dfc4602672294ab2de" />
+
+{/* Other components go here */}
+
+</> ); }
